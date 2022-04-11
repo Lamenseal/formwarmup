ac7ecc3b3760b93639f8d1ef9143217f542e4e03
銘鴻 徐 <kartd0079818@gmail.com>
2022-04-11 16:47:10 +0800

    4/11

---
+++ formwarmup/1_OverView/FrmOverview.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace formwarmup.NewFolder1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tbpConnected = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button6 = new System.Windows.Forms.Button();
+            this.northwindDataSet1 = new formwarmup.NorthwindDataSet();
+            this.productsTableAdapter1 = new formwarmup.NorthwindDataSetTableAdapters.ProductsTableAdapter();
+            this.categoriesTableAdapter1 = new formwarmup.NorthwindDataSetTableAdapters.CategoriesTableAdapter();
+            this.button7 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tbpConnected.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connected";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(52, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Northwind products";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 23;
+            this.listBox1.Location = new System.Drawing.Point(41, 111);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(650, 280);
+            this.listBox1.TabIndex = 2;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tbpConnected);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 597);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tbpConnected
+            // 
+            this.tbpConnected.Controls.Add(this.listBox1);
+            this.tbpConnected.Controls.Add(this.button1);
+            this.tbpConnected.Location = new System.Drawing.Point(4, 22);
+            this.tbpConnected.Name = "tbpConnected";
+            this.tbpConnected.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpConnected.Size = new System.Drawing.Size(792, 571);
+            this.tbpConnected.TabIndex = 0;
+            this.tbpConnected.Text = "Connected";
+            this.tbpConnected.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button7);
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 571);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Disconnected";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(133, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(629, 185);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(52, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 38);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Northwind Categories";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(52, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 47);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Northwind UnitPrice >30";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(52, 174);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 42);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Adventure work 2019";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(50, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(715, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(133, 285);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(620, 236);
+            this.dataGridView2.TabIndex = 7;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(52, 304);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // northwindDataSet1
+            // 
+            this.northwindDataSet1.DataSetName = "NorthwindDataSet";
+            this.northwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productsTableAdapter1
+            // 
+            this.productsTableAdapter1.ClearBeforeFill = true;
+            // 
+            // categoriesTableAdapter1
+            // 
+            this.categoriesTableAdapter1.ClearBeforeFill = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(52, 334);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 597);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tbpConnected.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tbpConnected;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private NorthwindDataSet northwindDataSet1;
+        private NorthwindDataSetTableAdapters.ProductsTableAdapter productsTableAdapter1;
+        private NorthwindDataSetTableAdapters.CategoriesTableAdapter categoriesTableAdapter1;
+        private System.Windows.Forms.Button button7;
+    }
+}
---
+++ formwarmup/1_OverView/FrmOverview.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace formwarmup.NewFolder1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tabControl1.Dock = DockStyle.Fill;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //1 SqlConnection
+            //2 Command
+            //3 SqlDate Reader
+            //4 UI Control
+            
+            
+            
+            SqlConnection cnn = null;
+
+
+
+            try
+            {
+                cnn = new SqlConnection("Data Source =.; Initial Catalog = Northwind; Integrated Security = True");
+                cnn.Open();
+                SqlCommand com = new SqlCommand("Select*from Products", cnn);
+                SqlDataReader  dataReader= com.ExecuteReader();
+                
+                while (dataReader.Read())
+                {
+                    string s = $"{dataReader["ProductName"],-40}-{dataReader["UnitPrice"]:c2}";
+                    //listBox1.Items.Add(dataReader["ProductName"]+" "+ dataReader["UnitPrice"]);
+                    listBox1.Items.Add(s);
+                }
+                
+                cnn.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if(cnn != null)
+                {
+                    cnn.Close();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source =.; Initial Catalog = Northwind; Integrated Security = True");
+            SqlDataAdapter adapter = new SqlDataAdapter("select*from Products",conn);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            this.dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source =.; Initial Catalog = Northwind; Integrated Security = True");
+            SqlDataAdapter adapter = new SqlDataAdapter("select*from Categories", conn);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            this.dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source =.; Initial Catalog = Northwind; Integrated Security = True");
+            SqlDataAdapter adapter = new SqlDataAdapter("select*from Products where UnitPrice > 30", conn);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            this.dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //SqlConnection conn = new SqlConnection("Data Source =.; Initial Catalog = AdventureWorks2019; Integrated Security = True");
+            //SqlDataAdapter adapter = new SqlDataAdapter("select*from Product(Production) ", conn);
+            //DataSet ds = new DataSet();
+            //adapter.Fill(ds);
+            //this.dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.categoriesTableAdapter1.Fill(this.northwindDataSet1.Categories);
+            this.dataGridView2.DataSource = this.northwindDataSet1.Categories;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.productsTableAdapter1.Fill(this.northwindDataSet1.Products);
+            this.dataGridView2.DataSource = this.northwindDataSet1.Products;
+        }
+    }
+}
